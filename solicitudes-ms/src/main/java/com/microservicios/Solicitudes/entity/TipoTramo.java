7e3597be85047825930da0136a11f878214d0632
Facundo <facuiglescano14@gmail.com>
2025-11-13 21:48:28 -0300

    realizo la logica de cambio de estado para seguimiento de solicitud, falta implementar cambio de estado de los contenedores

--- solicitudes-ms/src/main/java/com/microservicios/Solicitudes/entity/TipoTramo.java
+++ solicitudes-ms/src/main/java/com/microservicios/Solicitudes/entity/TipoTramo.java
@@ -2,7 +2,7 @@ package com.microservicios.Solicitudes.entity;
 
 public enum TipoTramo {
     ORIGEN_DEPOSITO,
-    DESSTINO_DEPOSITO,
+    DEPOSITO_DEPOSITO,
     DEPOSITO_DESTINO,
     ORIGEN_DESTINO
 }
